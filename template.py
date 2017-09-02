11964f5c81f54813e38460ea5096f24ba8fedfbe
Sharon Cichelli <girlwritescode@gmail.com>
2017-09-02 16:44:28 -0500

    fix declaration of drawing size

--- template.py
+++ template.py
@@ -2,10 +2,10 @@ import svgwrite
 
 
 def draw_page_border(dwg):
-    dwg.add(dwg.rect(insert=(150,-3150), size=(2250,3000), stroke='blue', stroke_width='5', fill='none'))
+    dwg.add(dwg.rect(insert=(150,150), size=(2250,3000), stroke='blue', stroke_width='5', fill='none'))
 
 
-drawing = svgwrite.Drawing('template.svg', profile='tiny', height=3300, width=2550)
+drawing = svgwrite.Drawing('template.svg', profile='tiny', size=(2550, 3300))
 draw_page_border(drawing)
 
 drawing.save()
