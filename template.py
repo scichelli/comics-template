e5d33e324bebfa166ffb70ee3181639778e965fe
Sharon Cichelli <girlwritescode@gmail.com>
2017-09-02 22:04:55 -0500

    flipped rows 2 and 3 from 40/60 to 60/40 and vice versa

--- template.py
+++ template.py
@@ -17,16 +17,16 @@ def full_span_bottom(dwg):
 	draw_rect(dwg, .5, 8.075, 7.5, 2.425)
 
 def row2_left(dwg):
-	draw_rect(dwg, .5, 3.025, 3, 2.425)
+	draw_rect(dwg, .5, 3.025, 4.4, 2.425)
 
 def row2_right(dwg):
-	draw_rect(dwg, 3.6, 3.025, 4.4, 2.425)
+	draw_rect(dwg, 5, 3.025, 3, 2.425)
 
 def row3_left(dwg):
-	draw_rect(dwg, .5, 5.55, 4.4, 2.425)
+	draw_rect(dwg, .5, 5.55, 3, 2.425)
 
 def row3_right(dwg):
-	draw_rect(dwg, 5, 5.55, 3, 2.425)
+	draw_rect(dwg, 3.6, 5.55, 4.4, 2.425)
 
 drawing = svgwrite.Drawing('template.svg', profile='tiny', size=to_dpi(8.5, 11))
 full_span_top(drawing)
